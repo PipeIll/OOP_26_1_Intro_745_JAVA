cf7326d81f569f96517b19ba882ed5e698c55ea7
JPLangua41 <jplagunaf@udistrital.edu.co>
2026-02-25 17:18:44 -0500

    Area_De_Un_Circulo 1.3

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -260,7 +260,7 @@ Rock crushes Scissors
     }
 
     public double areaCirculo(double radio) {
-	    return (math.PI*radio);
+	    return (Math.PI*radio);
     }
 
     public String zoodiac(int day, int month) {
