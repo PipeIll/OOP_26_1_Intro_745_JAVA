d6c2f3a9d4957c8f04029393351dca74de1ae1df
FELIPE <aparocio07felipe@gmail.com>
2026-03-08 17:18:43 -0500

    Zodiaco

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -415,9 +415,18 @@ Rock crushes Scissors
     }
 
     public String zoodiac(int day, int month) {
-        return "";
+        if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) return "Aries";
+        if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) return "Tauro";
+        if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) return "Gemini";
+        if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) return "Cancer";
+        if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) return "Leo";
+        if ((month == 8 && day >= 24) || (month == 9 && day <= 22)) return "virgo";
+        if ((month == 9 && day >= 23) || (month == 10 && day <= 23)) return "Libra";
+        if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) return "Escorpio";
+        if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) return "Sagitario";
+        if ((month == 12 && day >= 22) || (month == 1 && day <= 20)) return "Capricornio";
+        if ((month == 1 && day >= 21) || (month == 2 && day <= 19)) return "Acuario";
+        if ((month == 2 && day >= 20) || (month == 3 && day <= 20)) return "Picis";
     }
-
-
 }
 
